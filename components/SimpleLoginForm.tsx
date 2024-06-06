10293a35daf32b20e68016c59c84722e9f3f5fe9
AMykolaD <andrusenkomykola@gmail.com>
2024-06-06 14:16:12 +0200

    Small fix of form

--- components/SimpleLoginForm.tsx
+++ components/SimpleLoginForm.tsx
@@ -1,66 +1,73 @@
-import {StyleSheet, View, TextInput, Button} from 'react-native';
+import {StyleSheet, View, TextInput, Button, Text} from 'react-native';
 import {useState} from "react";
 import {RectButton} from "react-native-gesture-handler";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { violet } from '@/constants/Colors';
+import {GestureHandlerRootView} from "react-native-gesture-handler";
 
 export function SimpleLoginForm() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(false);
     return (
-        <View style={styles.goodAlign}>
-            <View style={styles.textInputWrap}>
-                <TextInput
-                    placeholder="Email"
-                    placeholderTextColor={"gray"}
-                    onChangeText={(value) => setEmail(value)}
-                    style={styles.textInput}
-                />
+        <GestureHandlerRootView>
+            <View style={styles.goodAlign}>
+                <View style={styles.textInputWrap}>
+                    <TextInput
+                        placeholder="Email"
+                        placeholderTextColor={"gray"}
+                        onChangeText={(value) => setEmail(value)}
+                        style={styles.textInput}
+                    />
+                </View>
+                <View style={[styles.hidePasswordTextField, styles.textInputWrap]}>
+                    <TextInput
+                        secureTextEntry = {!showPassword}
+                        placeholder="Password"
+                        placeholderTextColor={"gray"}
+                        onChangeText={(value) => setPassword(value)}
+                        style={[styles.textInput,
+                            {borderBottomRightRadius: 0,
+                                borderTopRightRadius: 0}]}/>
+                    <Ionicons
+                        name={showPassword ? 'eye-off' : 'eye'}
+                        size={30}
+                        onPress={() => setShowPassword(!showPassword)}
+                        style={styles.showIcon}>
+                    </Ionicons>
+                </View>
+                <RectButton
+                    onPress={() => {}}
+                    style={styles.button}>
+                    <Text style={{color: "white"}}>Login</Text>
+                </RectButton>
             </View>
-            <View style={[styles.hidePasswordTextField, styles.textInputWrap]}>
-                <TextInput
-                    secureTextEntry = {!showPassword}
-                    placeholder="Password"
-                    placeholderTextColor={"gray"}
-                    onChangeText={(value) => setPassword(value)}
-                    style={[styles.textInput, {borderBottomRightRadius: 0, borderTopRightRadius: 0}]}/>
-                <Ionicons
-                    name={showPassword ? 'eye-off' : 'eye'}
-                    size={25}
-                    onPress={() => setShowPassword(!showPassword)}
-                    style={styles.showIcon}>
-                </Ionicons>
-            </View>
-            <RectButton
-                onPress={() => alert(`Works! ${email} ${password}`)}
-                style={styles.button}
-            >Login</RectButton>
-        </View>
+        </GestureHandlerRootView>
     );
 }
 
 const styles = StyleSheet.create({
     textInputWrap: {
         width: "100%",
+        height: 45,
         borderRadius: 15,
         borderWidth: 1,
-        marginBottom: 20
+        marginTop: 5,
+        marginBottom: 15
     },
     textInput: {
         borderRadius: 15,
         opacity: 0.9,
-        width: "100%",
+        width: "90%",
         height: "100%",
         padding: 10,
         fontSize: 16,
         borderWidth: 0,
-
     },
     button: {
         borderRadius: 10,
-        height: 50,
         fontSize: 18,
+        height: 50,
         color: "white",
         display: "flex",
         alignItems: "center",
@@ -71,7 +78,7 @@ const styles = StyleSheet.create({
         width: "90%",
         marginLeft: "auto",
         marginRight: "auto",
-        marginBottom: 20
+        height: 180
     },
     hidePasswordTextField: {
         flexDirection: "row",
