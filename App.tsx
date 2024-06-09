699d3f3fcf5fd651855aa684664471e10a5c7536
koejdga <108132458+koejdga@users.noreply.github.com>
2024-06-09 23:38:03 +0300

    :sparkles: Add settings screen

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Launch from "./app/screens/Launch";
 import Login from "./app/screens/Login";
 import OnboardingScreen from "./app/screens/OnboardingScreen";
+import Settings from "./app/screens/Settings";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,7 +11,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="OnboardingScreen"
+        initialRouteName="Settings"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Login" component={Login} />
@@ -20,6 +21,7 @@ const App = () => {
           initialParams={{ screenNumber: 1 }}
         />
         <Stack.Screen name="Launch" component={Launch} />
+        <Stack.Screen name="Settings" component={Settings} />
       </Stack.Navigator>
     </NavigationContainer>
   );
