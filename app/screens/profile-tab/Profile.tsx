375bdbf02b6eb70c408b91332d2a6837e669dc81
koejdga <108132458+koejdga@users.noreply.github.com>
2024-06-14 02:49:27 +0300

    :sparkles: Add username editting in profile tab

--- app/screens/profile-tab/Profile.tsx
+++ app/screens/profile-tab/Profile.tsx
@@ -1,37 +1,60 @@
-import { SafeAreaView, Text, View } from "react-native";
+import FormTextInput from "@/app/components/form-components/FormTextInput";
+import CheckIcon from "@/app/components/icons/CheckIcon";
+import RowInProfileScreen from "@/app/components/one-row/RowInProfileScreen";
+import { useState } from "react";
+import { Pressable, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import EditIconProfile from "../../components/icons/EditIconProfile";
 import LogoutIcon from "../../components/icons/LogoutIcon";
 import SettingsIcon from "../../components/icons/SettingsIcon";
 import UploadIcon from "../../components/icons/UploadIcon";
-import WalletIcon from "../../components/icons/WalletIcon";
 import { base, red } from "../../constants/Colors";
 import { FontNames, Fonts } from "../../constants/Fonts";
-import RowInProfileScreen from "@/app/components/one-row/RowInProfileScreen";
 
 const Profile = ({ navigation }) => {
+  const [editMode, setEditMode] = useState(false);
+  const [username, setUsername] = useState("Kate Johnson");
+
   return (
     <SafeAreaView style={{ backgroundColor: "#FFF", flex: 1 }}>
       <View
-        style={{
-          marginTop: 43,
-          marginHorizontal: 34,
-          marginBottom: 53,
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
+        style={[
+          styles.header,
+          editMode ? { gap: 30 } : { justifyContent: "space-between" },
+        ]}
       >
-        <View style={{ gap: 8 }}>
-          <Text
-            style={[Fonts[FontNames.BODY_3], { color: base.light.light20 }]}
-          >
-            Username
-          </Text>
-          <Text style={[Fonts[FontNames.TITLE_2], { color: base.dark.dark75 }]}>
-            Kate Johnson
-          </Text>
-        </View>
-        <EditIconProfile color={base.dark.dark50} />
+        {!editMode && (
+          <>
+            <View style={{ gap: 8 }}>
+              <Text
+                style={[Fonts[FontNames.BODY_3], { color: base.light.light20 }]}
+              >
+                Username
+              </Text>
+              <Text
+                style={[Fonts[FontNames.TITLE_2], { color: base.dark.dark75 }]}
+              >
+                {username}
+              </Text>
+            </View>
+            <Pressable onPress={() => setEditMode(true)}>
+              <EditIconProfile color={base.dark.dark50} />
+            </Pressable>
+          </>
+        )}
+
+        {editMode && (
+          <>
+            <FormTextInput
+              placeholder="Enter username..."
+              value={username}
+              onChangeText={(value) => setUsername(value)}
+              style={{ flex: 1, marginHorizontal: 0, marginBottom: 0 }}
+            />
+            <Pressable onPress={() => setEditMode(false)}>
+              <CheckIcon color={base.light.light20} size={30} />
+            </Pressable>
+          </>
+        )}
       </View>
 
       <RowInProfileScreen
@@ -57,5 +80,15 @@ const Profile = ({ navigation }) => {
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  header: {
+    marginTop: 43,
+    marginHorizontal: 34,
+    marginBottom: 53,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
 
 export default Profile;
