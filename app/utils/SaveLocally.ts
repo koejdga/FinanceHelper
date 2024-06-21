48d103ef19a3d88bf0444134116214cdaa0e26ef
koejdga <108132458+koejdga@users.noreply.github.com>
2024-06-22 02:20:11 +0300

    :sparkles: Save expense goal for month and show expense progress on pie chart

--- app/utils/SaveLocally.ts
+++ app/utils/SaveLocally.ts
@@ -6,23 +6,39 @@ export const saveData = async (key: string, value: string) => {
       console.log(
         "ERROR: value for saving in AsyncStorage is undefined or null"
       );
-      return;
+      return false;
     }
     await AsyncStorage.setItem(key, JSON.stringify(value));
-    console.log(`${key} saved successfully!`);
+    console.log(`${key} with value ${value} saved successfully!`);
+    return true;
   } catch (e) {
     console.error(`Failed to save ${key}.`, e);
+    return false;
   }
 };
 
 export const loadData = async (key: string) => {
   try {
     const value = await AsyncStorage.getItem(key);
+    console.log(`Loaded ${key} with value ${value}`);
     if (value !== null) {
       return JSON.parse(value);
+    } else {
+      return null;
     }
-    console.log("No data found for key:", key);
   } catch (e) {
     console.error(`Failed to load ${key}.`, e);
+  }
+};
+
+export const removeValue = async (key: string) => {
+  try {
+    await AsyncStorage.removeItem(key);
+    console.log(`${key} removed`);
+    return true;
+  } catch (e) {
+    console.log(`Unable to remove ${key}`);
+    console.log(e);
+    return false;
   }
 };
